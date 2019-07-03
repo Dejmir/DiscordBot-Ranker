08bb1f1204d99d1c862d76ecdf3c31925b86c5bb
Dejmir <48758186+Dejmir@users.noreply.github.com>
2019-07-03 23:50:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const Bot = new Discord.Client();
 
 var port = process.env.PORT || 8080;
 var server=app.listen(port,function() {
-    
+console.log("app running on port 8080"); });
+
 const config = require("./settings.json");
 Bot.status = 1;
 
